c2f6670b41fad595b684186fed7da3a746202dca
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2022-09-18 14:44:41 +0700

    Item addons should now return null owner To discern between Survivor item addons from Killer power addons

--- Icon/Addon.cs
+++ Icon/Addon.cs
@@ -20,6 +20,8 @@ public partial class Addon : ObservableObject, IBasic, IFolder
     [ObservableProperty]
     private string _for;
 
+#nullable enable
     [ObservableProperty]
-    private string owner;
+    private string? owner;
+#nullable disable
 }
